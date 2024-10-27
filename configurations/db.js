13f46e846c73378504abc9231d6339f12e342410
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-10-27 23:32:08 +0530

    authentication

--- configurations/db.js
+++ configurations/db.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const uri = "";
-    await mongoose.connect(uri);
-    console.log('MongoDB connected successfully');
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(error.message);
     process.exit(1);
   }
 };
