c7840be70f68741f838f073bb183483d2298c799
Sharad Parmar <sharadparamr4142@gmail.com>
2024-10-28 22:07:33 +0530

    done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./configurations/db");
 
+const authRoutes=require("./services/authentication/routes/authRoutes")
 dotenv.config();
 
 connectDB();
@@ -9,6 +10,6 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
--- configurations/db.js
+++ configurations/db.js
@@ -1,14 +1,11 @@
-require('dotenv').config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected");
-  } catch (error) {
+  } 
+  catch (error) {
     console.error(error.message);
     process.exit(1);
   }
